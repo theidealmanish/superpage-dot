b60110d2aa6edb9331ff7fe9f3f882c0f31ac9ad
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-06-07 21:33:44 +0545

    add abis

---
+++ contracts/config.ts
@@ -0,0 +1,4 @@
+export const FACTORY_ADDRESS = '0xE5E32Abe70917cB091e33e46cCF7c91ec2Df8fc3';
+
+export { abi as CreatorTokenFactoryABI } from '@/contracts/abis/SuperPageCreatorTokenFactory.json';
+export { abi as CreatorTokenABI } from '@/contracts/abis/SuperPageCreatorToken.json';
